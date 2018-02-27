07590c0aa8de6f7da36723e526f6ea2cee64910d
Paul Saxe <paul@thesaxes.net>
2018-02-27 16:57:22 -0500

    Putting the imports needed into __init__.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -5,3 +5,10 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+# Bring up the classes so that they appear to be directly in
+# the {{ cookiecutter.project_slug }} package.
+
+from {{ cookiecutter.project_slug }}.packmol import {{ cookiecutter.step }}  # nopep8
+from {{ cookiecutter.project_slug }}.{{ cookiecutter.project_slug }} import {{ cookiecutter.step }}Step  # nopep8
+from {{ cookiecutter.project_slug }}.tk_packmol import Tk{{ cookiecutter.step }}  # nopep8
