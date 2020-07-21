9bcc718366b85707c063414544b455d9ce3ecd4d
Paul Saxe <paul@thesaxes.net>
2020-07-21 11:25:04 -0400

    Fixed using 'sw' as both a variable and import, & fixed other small errors.

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tk_{{cookiecutter.repo_name[0:-5]}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tk_{{cookiecutter.repo_name[0:-5]}}.py
@@ -3,7 +3,8 @@
 
 import seamm
 from seamm_util import ureg, Q_, units_class  # noqa: F401
-import seamm_widgets as sw
+{%- if cookiecutter.use_subflowchart == 'n' %}
+import seamm_widgets as sw{%- endif %}
 import {{ cookiecutter.repo_name }}  # noqa: F401
 import Pmw
 import pprint  # noqa: F401
@@ -104,7 +105,8 @@ class Tk{{ cookiecutter.class_name }}(seamm.TkNode):
             defaultbutton='OK',
             master=self.toplevel,
             title='Edit {{ cookiecutter.step }} step',
-            command=self.handle_dialog)
+            command=self.handle_dialog
+        )
         self.dialog.withdraw()
 
         # The information about widgets is held in self['xxxx'], i.e. this
@@ -117,12 +119,12 @@ class Tk{{ cookiecutter.class_name }}(seamm.TkNode):
 
 {%- if cookiecutter.use_subflowchart == 'y' %}
         # make it large!
-        sw = self.dialog.winfo_screenwidth()
-        sh = self.dialog.winfo_screenheight()
-        w = int(0.9 * sw)
-        h = int(0.8 * sh)
-        x = int(0.05 * sw / 2)
-        y = int(0.1 * sh / 2)
+        screen_w = self.dialog.winfo_screenwidth()
+        screen_h = self.dialog.winfo_screenheight()
+        w = int(0.9 * screen_w)
+        h = int(0.8 * screen_h)
+        x = int(0.05 * screen_w / 2)
+        y = int(0.1 * screen_h / 2)
 
         self.dialog.geometry('{}x{}+{}+{}'.format(w, h, x, y))
 
@@ -236,7 +238,8 @@ class Tk{{ cookiecutter.class_name }}(seamm.TkNode):
         if result != "OK":
             self.dialog.deactivate(result)
             raise RuntimeError(
-                "Don't recognize dialog result '{}'".format(result))
+                "Don't recognize dialog result '{}'".format(result)
+            )
 
         self.dialog.deactivate(result)
 
@@ -252,6 +255,7 @@ class Tk{{ cookiecutter.class_name }}(seamm.TkNode):
 {%- endif %}
 
 {%- if cookiecutter.use_subflowchart == 'y' %}
+
     def update_flowchart(self, tk_flowchart=None, flowchart=None):
         """Update the nongraphical flowchart.
 
