5ceac2cd15bb678544d74502ea2c171eae3e5ef1
Paul Saxe <paul@thesaxes.net>
2020-10-06 17:57:34 -0400

    Corrected initial comments.

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# {{ cookiecutter.repo_name }} documentation build configuration file, created by
-# sphinx-quickstart on Tue Jul  9 22:26:36 2013.
+# {{ cookiecutter.repo_name }} documentation build configuration file,
+# created by the SEAMM step cookiecutter.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
