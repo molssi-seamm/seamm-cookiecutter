70c67a7b32c4c25197a35477779ed5675488e4c4
Paul Saxe <paul@thesaxes.net>
2021-10-13 18:31:07 -0400

    Added test doing nothing so one test runs.

---
+++ tests/test_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `seamm-cookiecutter` package."""
+
+import pytest  # noqa: F401
+
+
+def test_nothing():
+    """Just create an object and test its type."""
+    assert True
